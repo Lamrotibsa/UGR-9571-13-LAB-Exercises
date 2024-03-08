b3170b5ae485933ad68d58b78d5cac683026dbe1
Lamrotibsa <lamrotibsa@gmail.com>
2024-03-08 04:59:21 -0800

    Dart lab

---
+++ LAB-1-Variables -and-Data-Types.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Create variables
+  String name = "lamrot";
+  int age = 22;
+  String favoriteColor = "blue";
+
+  // Print using string interpolation
+  print("My name is $name, I am $age years old, and my favorite color is $favoriteColor.");
+}
